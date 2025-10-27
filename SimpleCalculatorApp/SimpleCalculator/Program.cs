4f1a9c2584c8fd776720205dc9032fdacf6b079c
Josh Pewtress <61856857+JoshPewtress@users.noreply.github.com>
2025-10-27 12:21:02 -0600

    feat: Add SplitInput Method - Splits raw string by comma and trims whitespace from each entry

--- SimpleCalculatorApp/SimpleCalculator/Program.cs
+++ SimpleCalculatorApp/SimpleCalculator/Program.cs
@@ -1,1 +1,8 @@
 ﻿
+
+List<string> SplitInput(string input)
+{
+    return input.Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Trim())
+                .ToList();
+}
