d13c3e0b39282aac8cbe52599fdd1542c9eaabcb
Josh Pewtress <61856857+JoshPewtress@users.noreply.github.com>
2025-10-27 15:27:34 -0600

    fix(exception handling): Move usage loop to method with try catch in loop

--- SimpleCalculatorApp/SimpleCalculator/Program.cs
+++ SimpleCalculatorApp/SimpleCalculator/Program.cs
@@ -1,6 +1,4 @@
-﻿try
-{
-    Console.WriteLine(
+﻿Console.WriteLine(
         """
         Simple Calculator Application
 
@@ -9,17 +7,7 @@
         """
     );
 
-    do
-    {
-        Console.Write("\nEnter the equation: ");
-        Console.WriteLine($"The answer is: {Calculate(SplitInput(Console.ReadLine()))}."); 
-    } while (ContinueUsing());
-}
-catch (Exception e)
-{
-    Console.WriteLine();
-    throw;
-}
+PromptUser();
 
 List<string> SplitInput(string input)
 {
@@ -30,59 +18,51 @@ List<string> SplitInput(string input)
 
 double Calculate(List<string> inputs)
 {
-    try
-    {
-        List<double> numbers = inputs.Where((_, index) => index % 2 == 0)
+    List<double> numbers = inputs.Where((_, index) => index % 2 == 0)
                                  .Select(double.Parse)
                                  .ToList();
 
-        List<char> operands = inputs.Where((_, index) => index % 2 != 0)
-                                    .Select(ConvertOperator)
-                                    .ToList();
+    List<char> operands = inputs.Where((_, index) => index % 2 != 0)
+                                .Select(ConvertOperator)
+                                .ToList();
 
-        // PEMDAS Priority. Checks for and Handles */- operations
-        for (int i = 0; i < operands.Count;)
-        {
-            if (operands[ i ] is '*' or '/')
-            {
-                double left = numbers[ i ];
-                double right = numbers[ i + 1 ];
-
-                double result = operands[ i ] switch
-                {
-                    '*' => left * right,
-                    '/' => right != 0 ? left / right : throw new DivideByZeroException(),
-                    _ => throw new InvalidOperationException($"\"{operands[ i ]}\" is not a proper operand.")
-                };
-
-                numbers[ i ] = result;
-                numbers.RemoveAt(i + 1);
-                operands.RemoveAt(i);
-            }
-            else
-                i++; // Operand is a + or -.
-        }
-
-        double total = numbers.First();
-
-        // Secondary PEMDAS Priority check
-        for (int i = 0; i < operands.Count; i++)
-        {
-            total = operands[ i ] switch
-            {
-                '+' => total + numbers[ i + 1 ],
-                '-' => total - numbers[ i + 1 ],
-                _ => total
-            };
-        }
-
-        return total;
-    }
-    catch (Exception e)
+    // PEMDAS Priority. Checks for and Handles */- operations
+    for (int i = 0; i < operands.Count;)
     {
-        Console.WriteLine(e.Message);
-        return 0;
+        if (operands[ i ] is '*' or '/')
+        {
+            double left = numbers[ i ];
+            double right = numbers[ i + 1 ];
+
+            double result = operands[ i ] switch
+            {
+                '*' => left * right,
+                '/' => right != 0 ? left / right : throw new DivideByZeroException(),
+                _ => throw new InvalidOperationException($"\"{operands[ i ]}\" is not a proper operand.")
+            };
+
+            numbers[ i ] = result;
+            numbers.RemoveAt(i + 1);
+            operands.RemoveAt(i);
+        }
+        else
+            i++; // Operand is a + or -.
     }
+
+    double total = numbers.First();
+
+    // Secondary PEMDAS Priority check
+    for (int i = 0; i < operands.Count; i++)
+    {
+        total = operands[ i ] switch
+        {
+            '+' => total + numbers[ i + 1 ],
+            '-' => total - numbers[ i + 1 ],
+            _ => total
+        };
+    }
+
+    return total;
 }
 
 char ConvertOperator(string operation)
@@ -117,5 +97,25 @@ bool ContinueUsing()
     else
     {
         return output = false;
+    }
+}
+
+void PromptUser()
+{
+    bool continueUsing = true;
+
+    while (continueUsing)
+    {
+        try
+        {
+            Console.Write("\nEnter the equation: ");
+            Console.WriteLine($"The answer is: {Calculate(SplitInput(Console.ReadLine()))}.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error: {e.Message}");
+        }
+
+        continueUsing = ContinueUsing();
     }
 }
