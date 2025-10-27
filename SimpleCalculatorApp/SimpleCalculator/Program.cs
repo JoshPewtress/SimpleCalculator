657bac3510958765f4923250c626965d90c723ff
Josh Pewtress <61856857+JoshPewtress@users.noreply.github.com>
2025-10-27 14:21:48 -0600

    feat: add ConvertOperator method - Converts string operands to char - Handles "x", "X" and "*" as multiplication

--- SimpleCalculatorApp/SimpleCalculator/Program.cs
+++ SimpleCalculatorApp/SimpleCalculator/Program.cs
@@ -6,3 +6,79 @@ List<string> SplitInput(string input)
                 .Select(x => x.Trim())
                 .ToList();
 }
+
+double Calculate(List<string> inputs)
+{
+    try
+    {
+        List<double> numbers = inputs.Where((_, index) => index % 2 == 0)
+                                 .Select(double.Parse)
+                                 .ToList();
+
+        List<char> operands = inputs.Where((_, index) => index % 2 != 0)
+                                    .Select(ConvertOperator)
+                                    .ToList();
+
+        // PEMDAS Priority. Checks for and Handles */- operations
+        for (int i = 0; i < operands.Count;)
+        {
+            if (operands[ i ] is '*' or '/')
+            {
+                double left = numbers[ i ];
+                double right = numbers[ i + 1 ];
+
+                double result = operands[ i ] switch
+                {
+                    '*' => left * right,
+                    '/' => right != 0 ? left / right : throw new DivideByZeroException(),
+                    _ => throw new InvalidOperationException($"\"{operands[ i ]}\" is not a proper operand.")
+                };
+
+                numbers[ i ] = result;
+                numbers.RemoveAt(i + 1);
+                operands.RemoveAt(i);
+            }
+            else
+                i++; // Operand is a + or -.
+        }
+
+        double total = numbers.First();
+
+        // Secondary PEMDAS Priority check
+        for (int i = 0; i < operands.Count; i++)
+        {
+            total = operands[ i ] switch
+            {
+                '+' => total + numbers[ i + 1 ],
+                '-' => total - numbers[ i + 1 ],
+                _ => total
+            };
+        }
+
+        return total;
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+        return 0;
+    }
+}
+
+char ConvertOperator(string operation)
+{
+    switch (operation)
+    {
+        case "+":
+            return '+';
+        case "-":
+            return '-';
+        case "x":
+        case "X":
+        case "*":
+            return '*';
+        case "/":
+            return '/';
+        default:
+            throw new InvalidOperationException($"\"{operation}\" is not a proper operand.");
+    }
+}
